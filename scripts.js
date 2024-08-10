cfd0bbfca1de106c3655fd95f45335265bb6ec07
Ghaith-Azar <azargaith1@gmail.com>
2024-08-10 15:54:08 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,32 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted!');
+});
+
+
+const features = document.querySelectorAll('.feature');
+
+features.forEach(feature => {
+    feature.addEventListener('mouseenter', () => {
+        feature.classList.add('active');
+    });
+
+    feature.addEventListener('mouseleave', () => {
+        feature.classList.remove('active');
+    });
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.menu');
+
+    menuToggle.addEventListener('click', function () {
+        menu.classList.toggle('active');
+        menuToggle.classList.toggle('active');
+    });
+});
+
+
+
